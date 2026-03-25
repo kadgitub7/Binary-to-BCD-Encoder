b0ff75b7ab5626e661be4a392f52d6ba9dc78398
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-03-25 08:02:55 -0400

    implemented logic for decimal to bcd converters

---
+++ binaryBCD.v
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+
+module binaryBCD(
+    input one,two,three,four,five,six,seven,eight,nine,
+    output D,
+    output C,
+    output B,
+    output A
+    );
+    
+    assign D = eight | nine;
+    assign C = four | five | six | seven;
+    assign B = two | three | six | seven;
+    assign A = one | three | five | seven | nine;
+    
+endmodule
